c40d48f92e915ab7dd2eaf7e84a2bf6e694e9d65
Junpei Kawamoto <kawamoto.junpei@gmail.com>
2016-07-23 02:12:03 -0400

    Fixed links.

--- loci.rb
+++ loci.rb
@@ -13,10 +13,10 @@ class Loci < Formula
   version "0.1.5"
 
   if Hardware.is_64_bit?
-    url "https://github.com/jkawamoto/loci/releases/download/0.1.5/loci_0.1.5_darwin_amd64.zip"
+    url "https://github.com/jkawamoto/loci/releases/download/v0.1.5/loci_0.1.5_darwin_amd64.zip"
     sha256 "f3537221623b812847ed9c2b5fe0b742ecf983cfd4dd380f2d4af323716828a4"
   else
-    url "https://github.com/jkawamoto/loci/releases/download/0.1.5/loci_0.1.5_darwin_386.zip"
+    url "https://github.com/jkawamoto/loci/releases/download/v0.1.5/loci_0.1.5_darwin_386.zip"
     sha256 "137ffb673b171492aa7afd262bf1e0c4ecaf0def0f17cf8b3f173cee75efffa5"
   end
 
