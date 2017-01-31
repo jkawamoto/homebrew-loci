c669b1c203c34d0f276b79d7a4eaa0e4a3f25344
Junpei Kawamoto <kawamoto.junpei@gmail.com>
2017-01-31 03:33:05 -0500

    Version 0.4.0.

--- loci.rb
+++ loci.rb
@@ -10,14 +10,14 @@
 class Loci < Formula
   desc "Testing remote CI scripts locally"
   homepage "https://github.com/jkawamoto/loci"
-  version "0.3.5"
+  version "0.4.0"
 
   if Hardware::CPU.is_64_bit?
-    url "https://github.com/jkawamoto/loci/releases/download/v0.3.5/loci_0.3.5_darwin_amd64.zip"
-    sha256 "78df1acc9bc168b7c4080189ff23dd39ea138896d3e348ae71fc489664e66779"
+    url "https://github.com/jkawamoto/loci/releases/download/v0.4.0/loci_0.4.0_darwin_amd64.zip"
+    sha256 "33c0dd942f71d1ddb5e0ff092940b120459e85374dcc7b1134752d17c9063e93"
   else
-    url "https://github.com/jkawamoto/loci/releases/download/v0.3.5/loci_0.3.5_darwin_386.zip"
-    sha256 "6e0973ce271816810bcddf4905504e17e8ac8197c7f47ff97b1581b11a5d1668"
+    url "https://github.com/jkawamoto/loci/releases/download/v0.4.0/loci_0.4.0_darwin_386.zip"
+    sha256 "647ce62cf80c1791f35c114e94878c8c1284fe47a68483cb42df880ad7d3dfa7"
   end
 
   def install
