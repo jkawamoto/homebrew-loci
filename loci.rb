6cd723b77f2e81c91368c740c088213d2ec81389
Junpei Kawamoto <kawamoto.junpei@gmail.com>
2017-07-18 16:42:39 -0400

    Version 0.5.2.

--- loci.rb
+++ loci.rb
@@ -11,18 +11,18 @@ require 'rbconfig'
 class Loci < Formula
   desc "Testing remote CI scripts locally"
   homepage "https://github.com/jkawamoto/loci"
-  version "0.5.1"
+  version "0.5.2"
 
   if Hardware::CPU.is_64_bit?
     case RbConfig::CONFIG['host_os']
     when /mswin|msys|mingw|cygwin|bccwin|wince|emc/
       :windows
     when /darwin|mac os/
-      url "https://github.com/jkawamoto/loci/releases/download/v0.5.1/loci_0.5.1_darwin_amd64.zip"
-      sha256 "2bf4f6eda0d5fe1b40ad398e896df79454d74f9eff36c182a43c3f042d6f0c36"
+      url "https://github.com/jkawamoto/loci/releases/download/v0.5.2/loci_0.5.2_darwin_amd64.zip"
+      sha256 "fa4d7ae11fc51adc5d24c1c3e11ea61fcbe918a8df43951119a71dd046dbf73f"
     when /linux/
-      url "https://github.com/jkawamoto/loci/releases/download/v0.5.1/loci_0.5.1_linux_amd64.tar.gz"
-      sha256 "870fb525539a5fdb9a5d4c293cec2b9670f589cc32ee6af9eaebde81409875f9"
+      url "https://github.com/jkawamoto/loci/releases/download/v0.5.2/loci_0.5.2_linux_amd64.tar.gz"
+      sha256 "2f613e50b147a3c1973e0bdc8c0eb5b2f490ae45a6749f9d34e57682a8f0bb51"
     when /solaris|bsd/
       :unix
     else
@@ -33,11 +33,11 @@ class Loci < Formula
     when /mswin|msys|mingw|cygwin|bccwin|wince|emc/
       :windows
     when /darwin|mac os/
-      url "https://github.com/jkawamoto/loci/releases/download/v0.5.1/loci_0.5.1_darwin_386.zip"
-      sha256 "3da405181bf107586cec463c6cedf173fffec6345e143292c12fd2baf72b1742"
+      url "https://github.com/jkawamoto/loci/releases/download/v0.5.2/loci_0.5.2_darwin_386.zip"
+      sha256 "71eb8de5a7c6a69a4da456658df269a257d672aa857fa0599271ca49bfbfa3cc"
     when /linux/
-      url "https://github.com/jkawamoto/loci/releases/download/v0.5.1/loci_0.5.1_linux_386.tar.gz"
-      sha256 "477159a25c26004deb1ce8549aa4595e181258217fd23dd267bb629a55d75e41"
+      url "https://github.com/jkawamoto/loci/releases/download/v0.5.2/loci_0.5.2_linux_386.tar.gz"
+      sha256 "a2f40dc973e69f7a64f28e128c5265052a7761afe80052390c8cf9515b9f4aa6"
     when /solaris|bsd/
       :unix
     else
