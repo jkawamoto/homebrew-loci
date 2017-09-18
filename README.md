543208659006ec50670aeb0a0a8d7b6f641a2015
Junpei Kawamoto <kawamoto.junpei@gmail.com>
2017-09-18 14:18:23 -0400

    Version 0.5.3.

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 [![MIT License](http://img.shields.io/badge/license-MIT-blue.svg?style=flat)](LICENSE)
 [![Build Status](https://travis-ci.org/jkawamoto/loci.svg?branch=master)](https://travis-ci.org/jkawamoto/loci)
 [![Code Climate](https://codeclimate.com/github/jkawamoto/loci/badges/gpa.svg)](https://codeclimate.com/github/jkawamoto/loci)
-[![Release](https://img.shields.io/badge/release-0.5.2-brightgreen.svg)](https://github.com/jkawamoto/loci/releases/tag/v0.5.2)
+[![Release](https://img.shields.io/badge/release-0.5.3-brightgreen.svg)](https://github.com/jkawamoto/loci/releases/tag/v0.5.3)
 
 [![Loci Logo](https://jkawamoto.github.io/loci/img/image.png)](https://jkawamoto.github.io/loci/)
 
--- loci.rb
+++ loci.rb
@@ -11,18 +11,18 @@ require 'rbconfig'
 class Loci < Formula
   desc "Testing remote CI scripts locally"
   homepage "https://github.com/jkawamoto/loci"
-  version "0.5.2"
+  version "0.5.3"
 
   if Hardware::CPU.is_64_bit?
     case RbConfig::CONFIG['host_os']
     when /mswin|msys|mingw|cygwin|bccwin|wince|emc/
       :windows
     when /darwin|mac os/
-      url "https://github.com/jkawamoto/loci/releases/download/v0.5.2/loci_0.5.2_darwin_amd64.zip"
-      sha256 "fa4d7ae11fc51adc5d24c1c3e11ea61fcbe918a8df43951119a71dd046dbf73f"
+      url "https://github.com/jkawamoto/loci/releases/download/v0.5.3/loci_0.5.3_darwin_amd64.zip"
+      sha256 "55aae4ee99eaf99d9c6219bb02a6ec343772d9b22b8e2b722fe33d8ee3e00eda"
     when /linux/
-      url "https://github.com/jkawamoto/loci/releases/download/v0.5.2/loci_0.5.2_linux_amd64.tar.gz"
-      sha256 "2f613e50b147a3c1973e0bdc8c0eb5b2f490ae45a6749f9d34e57682a8f0bb51"
+      url "https://github.com/jkawamoto/loci/releases/download/v0.5.3/loci_0.5.3_linux_amd64.tar.gz"
+      sha256 "593316f7f16ab57945c08d6d08b8138dae5257eeeb14ebc2b67cb55a086bd41e"
     when /solaris|bsd/
       :unix
     else
@@ -33,11 +33,11 @@ class Loci < Formula
     when /mswin|msys|mingw|cygwin|bccwin|wince|emc/
       :windows
     when /darwin|mac os/
-      url "https://github.com/jkawamoto/loci/releases/download/v0.5.2/loci_0.5.2_darwin_386.zip"
-      sha256 "71eb8de5a7c6a69a4da456658df269a257d672aa857fa0599271ca49bfbfa3cc"
+      url "https://github.com/jkawamoto/loci/releases/download/v0.5.3/loci_0.5.3_darwin_386.zip"
+      sha256 "81bef19eaf6b7a63bd93f056ebd81db791593a9821d53fef7c5d1d27ec4ab929"
     when /linux/
-      url "https://github.com/jkawamoto/loci/releases/download/v0.5.2/loci_0.5.2_linux_386.tar.gz"
-      sha256 "a2f40dc973e69f7a64f28e128c5265052a7761afe80052390c8cf9515b9f4aa6"
+      url "https://github.com/jkawamoto/loci/releases/download/v0.5.3/loci_0.5.3_linux_386.tar.gz"
+      sha256 "7fff8030730f7eb4a98c0c534f2f4bd9ce1067c0cc482bc027bde32b43a6e49d"
     when /solaris|bsd/
       :unix
     else
